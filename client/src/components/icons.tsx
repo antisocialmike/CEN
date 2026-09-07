8916bf61ef1a91ff920827881515e71a8fa68f7a
antisocialmike <migueldelgadillo216@gmail.com>
2026-09-07 16:11:35 -0600

    actualizo los iconos propios de la marca

--- client/src/components/icons.tsx
+++ client/src/components/icons.tsx
@@ -1,75 +1,15 @@
+import { useId } from "react";
+
 interface IconProps {
   className?: string;
 }
 
-export function MailIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3 5.5C3 4.67157 3.67157 4 4.5 4H15.5C16.3284 4 17 4.67157 17 5.5V14.5C17 15.3284 16.3284 16 15.5 16H4.5C3.67157 16 3 15.3284 3 14.5V5.5Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-      />
-      <path d="M4 5.5L10 10.5L16 5.5" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-    </svg>
-  );
-}
-
-export function LockIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="4.5" y="9" width="11" height="7.5" rx="1.5" stroke="currentColor" strokeWidth="1.4" />
-      <path
-        d="M6.5 9V6.5C6.5 4.567 8.067 3 10 3C11.933 3 13.5 4.567 13.5 6.5V9"
-        stroke="currentColor"
-        strokeWidth="1.4"
-      />
-    </svg>
-  );
-}
-
-export function EyeIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M1.5 10C1.5 10 4.5 4.5 10 4.5C15.5 4.5 18.5 10 18.5 10C18.5 10 15.5 15.5 10 15.5C4.5 15.5 1.5 10 1.5 10Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-      />
-      <circle cx="10" cy="10" r="2.5" stroke="currentColor" strokeWidth="1.4" />
-    </svg>
-  );
-}
-
-export function EyeOffIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M1.5 10C1.5 10 4.5 4.5 10 4.5C15.5 4.5 18.5 10 18.5 10C18.5 10 15.5 15.5 10 15.5C4.5 15.5 1.5 10 1.5 10Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-      />
-      <circle cx="10" cy="10" r="2.5" stroke="currentColor" strokeWidth="1.4" />
-      <path d="M3 17L17 3" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-    </svg>
-  );
-}
-
-export function UsersIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <circle cx="7.5" cy="6.5" r="2.5" stroke="currentColor" strokeWidth="1.4" />
-      <path d="M2.5 16C2.5 12.9624 4.79 10.5 7.5 10.5C10.21 10.5 12.5 12.9624 12.5 16" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-      <circle cx="14" cy="7" r="2" stroke="currentColor" strokeWidth="1.4" />
-      <path d="M13 10.6C15.3 10.9 17.5 13 17.5 16" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-    </svg>
-  );
-}
-
 export function LogoMark({ className }: IconProps) {
+  const gradientId = `logo-grad-${useId()}`;
+
   return (
     <svg className={className} viewBox="0 0 32 32" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <rect x="1" y="1" width="30" height="30" rx="9" fill="url(#logo-grad)" />
+      <rect x="1" y="1" width="30" height="30" rx="9" fill={`url(#${gradientId})`} />
       <path
         d="M20.5 11.5C19.4 10.2 17.8 9.5 16 9.5C12.7 9.5 10 12.2 10 15.5C10 18.8 12.7 21.5 16 21.5C17.8 21.5 19.4 20.8 20.5 19.5"
         stroke="white"
@@ -79,101 +19,11 @@ export function LogoMark({ className }: IconProps) {
       <path d="M9.5 14H17.5" stroke="white" strokeWidth="2" strokeLinecap="round" />
       <path d="M9.5 17H17.5" stroke="white" strokeWidth="2" strokeLinecap="round" />
       <defs>
-        <linearGradient id="logo-grad" x1="1" y1="1" x2="31" y2="31" gradientUnits="userSpaceOnUse">
+        <linearGradient id={gradientId} x1="1" y1="1" x2="31" y2="31" gradientUnits="userSpaceOnUse">
           <stop stopColor="#34E4A6" />
           <stop offset="1" stopColor="#7C6CF6" />
         </linearGradient>
       </defs>
-    </svg>
-  );
-}
-
-export function BoltIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M11.2 2.5L4.5 11.5H9.3L8.5 17.5L15.5 8H10.5L11.2 2.5Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-        strokeLinejoin="round"
-      />
-    </svg>
-  );
-}
-
-export function ShieldIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M10 2.5L16.5 5V9.6C16.5 13.4 13.7 16.4 10 17.5C6.3 16.4 3.5 13.4 3.5 9.6V5L10 2.5Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-        strokeLinejoin="round"
-      />
-      <path d="M7.3 10L9.2 11.9L12.7 8.2" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
-
-export function ReceiptIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M5 2.5H15V17.5L13 16L11 17.5L9 16L7 17.5L5 16V2.5Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-        strokeLinejoin="round"
-      />
-      <path d="M7.5 6.5H12.5" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-      <path d="M7.5 9.5H12.5" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
-    </svg>
-  );
-}
-
-export function WalletIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path
-        d="M3 6.5C3 5.39543 3.89543 4.5 5 4.5H15C16.1046 4.5 17 5.39543 17 6.5V14.5C17 15.6046 16.1046 16.5 15 16.5H5C3.89543 16.5 3 15.6046 3 14.5V6.5Z"
-        stroke="currentColor"
-        strokeWidth="1.4"
-      />
-      <path d="M13 11C13 10.1716 13.6716 9.5 14.5 9.5C15.3284 9.5 16 10.1716 16 11C16 11.8284 15.3284 12.5 14.5 12.5C13.6716 12.5 13 11.8284 13 11Z" fill="currentColor" />
-    </svg>
-  );
-}
-
-export function ArrowUpRightIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M6 14L14 6" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" />
-      <path d="M7.5 6H14V12.5" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
-
-export function CheckIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M4 10.5L8 14.5L16 5.5" stroke="currentColor" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
-
-export function ChevronDownIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M5 8L10 13L15 8" stroke="currentColor" strokeWidth="1.6" strokeLinecap="round" strokeLinejoin="round" />
-    </svg>
-  );
-}
-
-export function LogoutIcon({ className }: IconProps) {
-  return (
-    <svg className={className} viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
-      <path d="M8 17.5H4.5C3.94772 17.5 3.5 17.0523 3.5 16.5V3.5C3.5 2.94772 3.94772 2.5 4.5 2.5H8" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M13 14L17 10L13 6" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" strokeLinejoin="round" />
-      <path d="M17 10H8" stroke="currentColor" strokeWidth="1.4" strokeLinecap="round" />
     </svg>
   );
 }
