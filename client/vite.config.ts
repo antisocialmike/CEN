43e5d2c5d1db6450c6ea1d89ebb89b338762e3d5
antisocialmike <migueldelgadillo216@gmail.com>
2026-09-15 01:20:36 -0600

    cambio el procesador de css a lightningcss y dedupeo react

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -3,6 +3,12 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  css: {
+    transformer: "lightningcss"
+  },
+  resolve: {
+    dedupe: ["react", "react-dom"]
+  },
   server: {
     port: 5173
   },
